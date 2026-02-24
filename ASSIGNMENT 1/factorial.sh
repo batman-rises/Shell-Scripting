3251a214abffb2a5911f44a5fa5d5f4d6f4826f4
Binayak Panda <pbinayak604@gmail.com>
2026-02-24 20:40:41 +0530

    q2

---
+++ ASSIGNMENT 1/factorial.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read -p "enter a number" n
+fact=1
+if [ $n -lt 0 ]
+then
+        echo "enter a valid number"
+else
+        for((i=1;i<=n;i++))
+        do
+                fact=$((fact*i))
+        done
+        echo "factorial of $n is $fact"
+fi
